053e1a19dfbb0c2fbe2f662969db0c056022c368
dead-horse <dead_horse@qq.com>
2017-11-10 23:37:54 +0800

    f

--- test/test.js
+++ test/test.js
@@ -31,7 +31,7 @@ describe('fs.readdirSyncRecursive()', function () {
 
   it('should filter pass dir', function () {
     var files = read(path.join(__dirname, '..'), function (name, index, dir) {
-      return name[0] !== '.' && name !== 'node_modules' && name !== 'coverage' && dir !== __dirname &&  && name !== 'package-lock.json'
+      return name[0] !== '.' && name !== 'node_modules' && name !== 'coverage' && dir !== __dirname && name !== 'package-lock.json'
     })
 
     files.length.should.equal(4)
@@ -84,7 +84,7 @@ describe('fs.readdirSyncRecursive()', function () {
       fs.symlinkSync(path.join(__dirname, '..'), linkname, 'dir')
 
       var files = read(linkname, function (name) {
-        return name[0] !== '.' && name !== 'node_modules' && name !== 'coverage'  && name !== 'package-lock.json'
+        return name[0] !== '.' && name !== 'node_modules' && name !== 'coverage' && name !== 'package-lock.json'
       })
 
       files.length.should.equal(5)
